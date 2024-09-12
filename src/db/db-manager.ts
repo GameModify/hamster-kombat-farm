e15bbfb98ff2a1aafda12898854e456b4cb9e4ba
Serent <Serent2003@mail.ru>
2024-09-12 21:31:57 +0300

    update set next start time function update get available accounts

--- src/db/db-manager.ts
+++ src/db/db-manager.ts
@@ -8,9 +8,40 @@ export class DBManager {
         this.prisma = new PrismaClient()
     }
 
-    async getAvailableAccount() : Promise<Array<AccountInfo>> {
-        let accounts: Array<AccountInfo> = []
-        // todo: get available accounts from db
-        return accounts
+    async setNextStartTime(accountInfo: AccountInfo, nextStartTime: number): Promise<void> {
+        try {
+            await this.prisma.accounts.update({
+                where: {
+                    telegram_id: accountInfo.telegramID,
+                },
+                data: {
+                    next_start_timestamp: nextStartTime,
+                },
+            });
+            console.log(`Next start time for account ${accountInfo.telegramID} has been updated`);
+        } catch (error) {
+            console.error('Error updating next start time:', error);
+        }
+    }
+
+    async getAvailableAccounts(): Promise<AccountInfo[]> {
+        const now = Date.now();
+
+        const availableAccounts = await this.prisma.accounts.findMany({
+            where: {
+                next_start_timestamp: {
+                    lt: now,
+                },
+            },
+        });
+
+        return availableAccounts.map(account => ({
+            id: account.id,
+            telegramID: account.telegram_id,
+            username: account.username,
+            nextStartTimestamp: account.next_start_timestamp,
+            cookie: account.cookie,
+            headers: {},
+        }));
     }
 }
