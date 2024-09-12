fb6ec581fbf98a77eb1c9cf061265b4d4cc7d167
Serent <Serent2003@mail.ru>
2024-09-12 21:29:41 +0300

    update add dbmanager, add setNextStartTime

--- src/account-manager.ts
+++ src/account-manager.ts
@@ -1,21 +1,25 @@
 import {AccountInfo} from "./entities";
+import {DBManager} from "./db/db-manager";
 
 export declare type SuccessCallback = (account: AccountManager) => void
 export declare type ErrorCallback = (account: AccountManager, error: Error) => void
 
 export class AccountManager {
     private _accountInfo: AccountInfo;
+    private _dbManager: DBManager;
     protected _onSuccess: SuccessCallback;
     protected _onError: ErrorCallback
 
     constructor (private accountInfo: AccountInfo, onSuccess: SuccessCallback, onError: ErrorCallback) {
         this._accountInfo = accountInfo;
+        this._dbManager = new DBManager();
         this._onError = onError;
         this._onSuccess = onSuccess;
     }
 
     async task() {
-
+        // TODO: change 0 to real next starttime
+        await this._dbManager.setNextStartTime(this.accountInfo, 0)
         this._onSuccess(this)
     }
 
